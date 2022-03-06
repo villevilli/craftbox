d25561d14eb5ec70847ab61c06d9b096e4fa3295
Villsukka <kujala.ville.vk@gmail.com>
2022-03-06 21:44:41 +0200

    Initial Commit

---
+++ packages/craftbox_frontend/src/routes/index.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    let i = "among"
+</script>
+
+<h1 class="title">
+    Hello CraftBox
+</h1>
+
+<button on:click={() => alert(i)}>
+    among
+</button>
+
+<style lang="scss">
+    .title{
+        color: green;
+        &:hover{
+            background-color: orange;
+        }
+    }
+</style>
